afde10aa098d237e6196e122a8bf838dce91ea9a
ADMIN <adityakhade1718@gmail.com>
2026-05-08 18:25:31 +0530

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -15,29 +15,23 @@ if (
   delete process.env.HOST;
 }
 
-// Check if we are actually in a local development environment
-const isDev = process.env.NODE_ENV === "development";
-
 const host = new URL(process.env.SHOPIFY_APP_URL || "http://localhost").hostname;
+let hmrConfig;
 
-let hmrConfig = false; // Default: No WebSockets in production
-
-if (isDev) {
-  if (host === "localhost") {
-    hmrConfig = {
-      protocol: "ws",
-      host: "localhost",
-      port: 64999,
-      clientPort: 64999,
-    };
-  } else {
-    hmrConfig = {
-      protocol: "wss",
-      host: host,
-      port: parseInt(process.env.FRONTEND_PORT) || 8002,
-      clientPort: 443,
-    };
-  }
+if (host === "localhost") {
+  hmrConfig = {
+    protocol: "ws",
+    host: "localhost",
+    port: 64999,
+    clientPort: 64999,
+  };
+} else {
+  hmrConfig = {
+    protocol: "wss",
+    host: host,
+    port: parseInt(process.env.FRONTEND_PORT) || 8002,
+    clientPort: 443,
+  };
 }
 
 export default defineConfig({
